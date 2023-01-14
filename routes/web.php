70e2d1c0b76a7ad265e889cedf94ce245ee41e26
Aaron Pierson <aaron.pierson@proton.me>
2023-01-13 18:30:47 -0600

    setting up controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
+
 });
+
+Route::get('/users', 'UserController@index');
+
+Route::get('/users/{id}', 'UserController@show');
