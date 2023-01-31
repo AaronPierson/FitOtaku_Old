8718fd60d699c24efa6ba2bf461aaddf1db4cac6
Aaron Pierson <aaron.pierson@proton.me>
2023-01-30 19:10:30 -0600

    Inertia setup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,19 +5,21 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Inertia\Inertia;
+
 
 class UsersController extends Controller
 {
 
 
     public function index(){   
-        return view('users/dashboard', ['Player' => null]);
+        return Inertia::render('Dashboard', ['Player' => "Aaron"]);
     }
 
     public function show($id)
     {
         $user = User::find($id);
-        return view('users/dashboard', [
+        return Inertia::render('Dashboard', [
             'Player' => $user
         ]);
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,16 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import chart from './components/BarChart'
-import modal from './components/ModalBox'
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import NProgress from 'nprogress'
+import { router } from '@inertiajs/vue3'
 
-const app = createApp({
-    data() {
-        return {
-          showModal: false,
-        };
-      },
-      components: {
-        modal,
-      },
-})
-app.component('chart-grid', chart);
-app.component('modal-box', modal);
+createInertiaApp({
 
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+});
 
-app.mount('#app')
+// NProgress.init({ color: '#4B5563' });
