7b9c8812bdce8e548c41fb10bcefd8983b4421f0
Aaron Pierson <aaron.pierson@proton.me>
2023-02-03 22:50:00 -0600

    picocss install

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import NProgress from 'nprogress'
 import { router } from '@inertiajs/vue3'
+import '@picocss/pico'
 
 createInertiaApp({
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" /> --}}
     {{-- <link rel="stylesheet" href="/css/base.css" /> --}}
-    {{-- <link rel="stylesheet" href="css/pico.min.css"> --}}
     <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+
 
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/app.js') }}" defer></script>
